6fb9513b98905c5ee9f42bcc326d28adbc79cf7d
amyrbakhsh <amyr.bakhsh@gmail.com>
2024-12-04 11:27:08 +0300

    step 2

---
+++ js/app.js
@@ -0,0 +1,14 @@
+/*-------------- Constants -------------*/
+
+
+/*---------- Variables (state) ---------*/
+let state = {}
+
+/*----- Cached Element References  -----*/
+const textElement = document.querySelector('#board')
+const choiceButtonsElement = document.getElementById('choice-buttons')
+
+/*-------------- Functions -------------*/
+
+
+/*----------- Event Listeners ----------*/
